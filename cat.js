e58a48936bd34bbe336ecb7e16159b22e0286893
oOFaYOo <nika.tseleva@mail.ru>
2020-12-09 02:44:38 +0500

    Completed the registration form

---
+++ cat.js
@@ -0,0 +1,10 @@
+let addCat = document.getElementById("add_cat");
+let addNewAnimal = document.querySelector(".add_new_animal");
+addCat.onclick = () => {
+    addNewAnimal.hidden = false
+};
+
+let closeRegForm = document.getElementById("close_reg_form");
+closeRegForm.onclick = () => {
+    addNewAnimal.hidden = true
+};
