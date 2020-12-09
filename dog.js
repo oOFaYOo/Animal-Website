24fe920fada45548571da952b3fe6f251f74e96e
oOFaYOo <nika.tseleva@mail.ru>
2020-12-10 04:08:41 +0500

    fix bag

--- dog.js
+++ dog.js
@@ -9,24 +9,27 @@ closeRegForm.onclick = () => {
     addNewAnimal.hidden = true
 };
 
+
 let oldOwner = document.getElementById("old_owner");
 let newOwner = document.getElementById("new_owner");
 let descriptionNewOwner = document.getElementById("description_of_NewOwner");
 
 oldOwner.onchange = () => {
     descriptionNewOwner.hidden = true;
+    if(descriptionNewOwner.hidden === true){document.getElementById("phone_num").disabled = "disabled";}
+    if(descriptionNewOwner.hidden === true){document.getElementById("name_of_owner").disabled = "disabled";}
     document.querySelector(".phone_OldOwner").hidden = false;
-    document.getElementById("phone_num").disabled = "disabled";
-    document.getElementById("name_of_owner").disabled = "disabled";
-
+    document.getElementById("phone_OldOwner").removeAttribute("disabled");
 };
 newOwner.onchange = () => {
-    descriptionNewOwner.hidden = false;
+    if(descriptionNewOwner.hidden === false){
+        document.getElementById("phone_OldOwner").disabled = "disabled";
+    }descriptionNewOwner.hidden = false;
     document.querySelector(".phone_OldOwner").hidden = true;
     document.getElementById("phone_num").removeAttribute("disabled");
     document.getElementById("name_of_owner").removeAttribute("disabled");
-    document.getElementById("phone_OldOwner").disabled = "disabled";
 };
+
 
 document.getElementById("button_reset").onclick = () => {
     descriptionNewOwner.hidden = false;
