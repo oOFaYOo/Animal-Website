401a8ea128e10c41870985ec78a6785ca3a04545
oOFaYOo <nika.tseleva@mail.ru>
2020-12-13 17:01:03 +0500

    Parsing animals by types and checking the correctness of animal data

--- animal.js
+++ animal.js
@@ -61,18 +61,114 @@ document.getElementById("button_reset").onclick = () => {
 };
 
 
-let get_pets = new XMLHttpRequest();
+ let get_pets = new XMLHttpRequest();
 get_pets.open("get", "http://192.168.0.101:5000/pets",false);
-
 get_pets.send();
-// JSON.parse(get_pets.response);
-console.log(get_pets.response);
-class animal {
-    constructor(name, gender, breed, age){
+let allPetsWithTypeNum = JSON.parse(get_pets.response);
+
+let pets_type = new XMLHttpRequest();
+pets_type.open("get", "http://192.168.0.101:5000/pets/types", false);
+pets_type.send();
+let listOfPetsType = JSON.parse(pets_type.response);
+let allPetsWithoutFeathersColor = [];
+let allPets = [];
+
+
+let parrotFeathersColors = new XMLHttpRequest();
+parrotFeathersColors.open("get", "http://192.168.0.101:5000/pets/parrotFeathersColors", false);
+parrotFeathersColors.send();
+let feathersColors = JSON.parse(parrotFeathersColors.response);
+
+function changeType (arr, obj) {
+    for (let key in obj) {
+        for (let part of arr) {
+            if (`${part.Type}` === key) {
+                part.Type = obj[key];
+                allPetsWithoutFeathersColor.push(part);
+            }
+        }
+   }
+}
+function choiceFeathersColor (arr, obj) {
+    for (let key in obj) {
+        for (let part of arr) {
+            if (`${part.FeathersColor}` === key) {
+                part.FeathersColor = obj[key];
+            }
+            allPets.push(part);
+        }
+    }
+}
+
+changeType(allPetsWithTypeNum, listOfPetsType);
+choiceFeathersColor(allPetsWithoutFeathersColor, feathersColors)
+
+class Animals {
+    constructor(type, name, breed, age){
+    this.type = type;
     this.name = name;
-    this.gender = gender;
     this.breed = breed;
     this.age = age;
 }
 }
+class Cats extends Animals{
+}
+class Dogs extends Animals{
+    constructor(type, name, breed, age, size){
+        super(type, name, breed, age);
+        this.size = size;
+    }
+}
+class Parrots extends Animals{
+    constructor(type, name, breed, age, color){
+        super(type, name, breed, age);
+        this.color = color;
+    }
+}
 
+function IdentifyAnimal(arrOfObj) {
+    for (let part of arrOfObj) {
+        switch (part.Type) {
+            case "Cat":
+                UnvalidatedCats.push(new Cats(part.Type, part.Name, part.Breed, part.Age));
+                break;
+            case "Dog":
+                UnvalidatedDogs.push(new Dogs(part.Type, part.Name, part.Breed, part.Age, part.IsBigDog));
+                break;
+            case "Parrot":
+                UnvalidatedParrots.push(new Parrots(part.Type, part.Name, part.Breed, part.Age, part.FeathersColor));
+                break;
+        }
+    }
+}
+
+let UnvalidatedCats = [];
+let UnvalidatedDogs = [];
+let UnvalidatedParrots = [];
+
+IdentifyAnimal(allPets);
+
+let cats =[];
+let dogs = [];
+let parrots = [];
+
+function animalValidation(arrOfAnimal) {
+    for (let part of arrOfAnimal) {
+        if (part.name !== null && part.name !== " " && part.name !== "" && isNaN(part.name) && part.breed !== " " && part.breed !== "" && isNaN(part.breed) && typeof (part.age) === "number") {
+            if (part.type === "Cat")
+                cats.push(part);
+            if (part.type === "Dog")
+                dogs.push(part);
+            if (part.type === "Parrot")
+                parrots.push(part);
+        }
+    }
+}
+
+animalValidation(UnvalidatedCats);
+animalValidation(UnvalidatedDogs);
+animalValidation(UnvalidatedParrots);
+
+console.log(cats);
+console.log(dogs);
+console.log(parrots);
